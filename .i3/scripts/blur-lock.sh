41b877191c9861228fdb8f0ac8c8b08d318923a0
Carl Kershaw <ckershaw@umich.edu>
2016-03-14 14:44:26 -0400

    small changes

--- .i3/scripts/blur-lock.sh
+++ .i3/scripts/blur-lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 3% -scale 3400% /tmp/screen_locked2.png
+convert /tmp/screen_locked.png -scale 4% -scale 2500% /tmp/screen_locked2.png
 mv /tmp/screen_locked2.png /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
