9e5adf8ea01fafb573d8a5bef7092c3397766d37
Carl Kershaw <ckershaw@google.com>
2017-01-30 09:07:35 -0800

    undo mogrify

--- .i3/scripts/blur-lock.sh
+++ .i3/scripts/blur-lock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 scrot /tmp/screen_locked.png
-mogrify -scale 4% -scale 2500% /tmp/screen_locked.png
+convert /tmp/screen_locked.png -scale 4% -scale 2502% /tmp/screen_locked2.png
+mv /tmp/screen_locked2.png /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
